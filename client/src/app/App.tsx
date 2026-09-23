b25e997e5f8bfc4ee79464a38c76b981307b9b6b
pramodboda <pramodkumarboda@gmail.com>
2026-09-23 19:48:23 +0530

    UPDATE: Project Setup

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import heroImg from './../assets/hero.png'
 import reactLogo from './../assets/react.svg'
 import viteLogo from './../assets/vite.svg'
-import './App.css'
+
+
+import ClimateScope from "../dashboards/ClimateScope/ClimateScope";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
+    <ClimateScope/>
+      {/* <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
@@ -114,7 +117,7 @@ function App() {
       </section>
 
       <div className="ticks"></div>
-      <section id="spacer"></section>
+      <section id="spacer"></section> */}
     </>
   )
 }
