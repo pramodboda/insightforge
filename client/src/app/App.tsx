e166679eea96eadf4f3abccbb8a8e5f5b648f5b2
pramodboda <pramodkumarboda@gmail.com>
2026-09-24 14:37:25 +0530

    UPDATE: Project Setup

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -23,7 +23,6 @@ function App() {
         <li>CommercePulse</li>
         <li>HealthMetrics</li>
         <li>GlobalRisk Monitor</li>
-        <li>Developer Analytics</li>
       </ul>
 
       <ColorRule />
